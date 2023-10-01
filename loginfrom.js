17d9cc56a33c2e3ae8d5e3ed07c310191bed4e48
Gautam Pandya <121717699+Itspandya@users.noreply.github.com>
2023-10-01 04:17:27 -0400

    Add files via upload

---
+++ loginfrom.js
@@ -0,0 +1,45 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('h2').textillate({ in: { effect: 'fadeInLeftBig' } });
+
+
